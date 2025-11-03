b9457650bb71aa32d295d4704ba1507dfe7ab9fc
Aadinath <aadinathchaudhari47@gmail.com>
2025-11-04 04:07:42 +0530

    login page added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,45 +1,43 @@
 class UsersController < ApplicationController
 
+   def show 
+      @user=User.find(params[:id])
+      # @articles= @user.articles
+      @articles= @user.articles.paginate(page: params[:page], per_page: 5)
+   end   
 
-    def show 
-         @user=User.find(params[:id])
-        # @articles= @user.articles
-         @articles= @user.articles.paginate(page: params[:page], per_page: 5)
-    end   
+   def index
+      #@user= User.all
+      @user=User.paginate(page: params[:page], per_page: 5)
+   end
 
-    def index
-         #@user= User.all
-         @user=User.paginate(page: params[:page], per_page: 5)
-    end
+   def new 
+      @user= User.new            
+   end    
+   
+   def edit
+      @user=User.find(params[:id])        
+   end
 
-    def new 
-       @user= User.new            
-    end    
-    
-    def edit
-        @user=User.find(params[:id])
-        
-    end
+   def update 
+      @user=User.find(params[:id])
+      if @user.update(user_params)
+      flash[:notice] = "your account information was successfully updated"
+      redirect_to @user
+      else
+      render 'edit' 
+      end
+   end    
 
-    def update 
-       @user=User.find(params[:id])
-       if @user.update(user_params)
-        flash[:notice] = "your account information was successfully updated"
-        redirect_to @user
-       else
-        render 'edit' 
-       end
-    end    
-
-    def create
-       @user= User.new(user_params)
-       if @user.save
-        flash[:notice]="Welcome to Aplha Blog #{@user.username},you have successfully signed in "
-        redirect_to articles_path
-       else
-        render 'new'
-       end 
-    end    
+   def create
+      @user= User.new(user_params)
+      if @user.save
+      flash[:notice]="Welcome to Aplha Blog #{@user.username},you have successfully signed in "
+      redirect_to articles_path
+      else
+      render 'new'
+      end 
+   end    
 
 
     private
